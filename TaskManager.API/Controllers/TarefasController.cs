7ad808efb3e67e7a9aabda417e2ab634586dd6ce
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-05-27 21:37:12 -0300

    feat: implementar TarefasController e lógica de serviço para gerenciamento de tarefas

---
+++ TaskManager.API/Controllers/TarefasController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.Interfaces;
+using TaskManager.Application.Models;
+
+namespace TaskManager.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TarefasController : ControllerBase
+{
+    private readonly ITarefaService _service;
+
+    public TarefasController(ITarefaService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _service.GetAllAsync();
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var result = await _service.GetByIdAsync(id);
+
+        if (!result.IsSuccess)
+            return NotFound(result);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateTarefaInputModel input)
+    {
+        var result = await _service.CreateAsync(input);
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = result.Data },
+            result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] UpdateTarefaInputModel input)
+    {
+        var result = await _service.UpdateAsync(id, input);
+
+        if (!result.IsSuccess)
+            return NotFound(result);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var result = await _service.DeleteAsync(id);
+
+        if (!result.IsSuccess)
+            return NotFound(result);
+
+        return NoContent();
+    }
+}
