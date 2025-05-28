15d9ae444280f00c345937ceff2e5aae2ffea8c8
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-05-27 21:21:41 -0300

    feat(infrastructure): configurar DbContext, repositories e mapeamentos

---
+++ TaskManager.Infrastructure/AppDbContext/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Entities;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext() { }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Tarefa> Tarefas { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarefa>()
+            .Property(t => t.Titulo)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
