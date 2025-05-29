75565ceec91ac923b9282bfa20c5fd9055b3847a
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-05-29 19:02:13 -0300

    feat: adiciona Validadores FluentValidation

---
+++ TaskManager.Application/Validators/CreateTarefaInputModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TaskManager.Application.Models;
+
+public class CreateTarefaInputModelValidator : AbstractValidator<CreateTarefaInputModel>
+{
+    public CreateTarefaInputModelValidator()
+    {
+        RuleFor(x => x.Titulo)
+            .NotEmpty().WithMessage("Título é obrigatório.")
+            .MaximumLength(100).WithMessage("Título não pode ter mais que 100 caracteres.");
+
+        RuleFor(x => x.Descricao)
+            .MaximumLength(500).WithMessage("Descrição não pode ter mais que 500 caracteres.");
+    }
+}
---
+++ TaskManager.Application/Validators/UpdateTarefaInputModelValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Application.Models;
+using TaskManager.Domain.Enums;
+
+namespace TaskManager.Application.Validators
+{
+    public class UpdateTarefaInputModelValidator : AbstractValidator<UpdateTarefaInputModel>
+    {
+        public UpdateTarefaInputModelValidator()
+        {
+            RuleFor(x => x.Titulo)
+                .NotEmpty().WithMessage("Título é obrigatório.")
+                .MaximumLength(100).WithMessage("Título não pode ter mais que 100 caracteres.");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("Status inválido.");
+
+            RuleFor(x => x.DataConclusao)
+                .NotNull()
+                .When(x => x.Status == StatusTarefa.Concluida)
+                .WithMessage("Data de conclusão é obrigatória para status 'Concluída'.");
+
+            RuleFor(x => x.DataConclusao)
+                .Null()
+                .When(x => x.Status != StatusTarefa.Concluida)
+                .WithMessage("Data de conclusão só deve ser informada para status 'Concluída'.");
+        }
+    }
+}
