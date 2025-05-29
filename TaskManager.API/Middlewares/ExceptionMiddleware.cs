dc14a2ab0f8a2f91efc74cff34869bbcbb9859af
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-05-28 22:41:19 -0300

    feat(api): adicionar middleware de tratamento global de exceções

---
+++ TaskManager.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace TaskManager.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = new
+                {
+                    status = context.Response.StatusCode,
+                    title = "Ocorreu um erro no servidor.",
+                    detail = _env.IsDevelopment() ? ex.Message : "Erro interno, contate o suporte.",
+                    stackTrace = _env.IsDevelopment() ? ex.StackTrace : null
+                };
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
